3537bcbb301704c335bd80c69720305b591dccae
kulikov-ivan <kulikovia.72@icloud.com>
2021-07-20 13:07:06 +0300

    add user properties setting

--- Sources/PaltaLib/Engine/PaltaLib+UserProperties.swift
+++ Sources/PaltaLib/Engine/PaltaLib+UserProperties.swift
@@ -4,7 +4,9 @@ extension PaltaLib {
 
     public typealias UserProperties = [AnyHashable: Any]
 
-    public func set(_ userProperties: UserProperties) {
-        
+    public func setUserProperties(_ userProperties: UserProperties) {
+        amplitureInstances.forEach {
+            $0.setUserProperties(userProperties)
+        }
     }
 }
