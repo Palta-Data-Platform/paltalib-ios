16a97a04478ed6a06ce43b9f2f4b8a6ac46309a1
kulikov-ivan <kulikovia.72@icloud.com>
2021-07-20 13:06:13 +0300

    add events logging

--- Sources/PaltaLib/Engine/PaltaLib+Event.swift
+++ Sources/PaltaLib/Engine/PaltaLib+Event.swift
@@ -5,12 +5,12 @@ extension PaltaLib {
     public struct Event {
 
         fileprivate let name: String
-        fileprivate let properties: [AnyHashable: Any]
+        fileprivate let properties: [AnyHashable: Any]?
         fileprivate let groups: [AnyHashable: Any]?
         fileprivate let outOfSession: Bool
 
         public init(name: String,
-                    properties: [AnyHashable: Any] = [:],
+                    properties: [AnyHashable: Any]? = nil,
                     groups: [AnyHashable: Any]? = nil,
                     outOfSession: Bool = false) {
             self.name = name
@@ -20,7 +20,24 @@ extension PaltaLib {
         }
     }
 
-    public func log(_ event: Event) {
+    public func logEvent(_ eventType: String,
+                         withEventProperties: [AnyHashable : Any]? = nil,
+                         withGroups: [AnyHashable : Any]? = nil,
+                         outOfSession: Bool = false) {
+        logEvent(.init(name: eventType,
+                       properties: withEventProperties,
+                       groups: withGroups,
+                       outOfSession: outOfSession))
+    }
 
+    public func logEvent(_ event: Event) {
+        amplitureInstances.forEach {
+            $0.logEvent(
+                event.name,
+                withEventProperties: event.properties,
+                withGroups: event.groups,
+                outOfSession: event.outOfSession
+            )
+        }
     }
 }
