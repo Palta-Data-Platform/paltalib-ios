c6b45f74472fb7cb0fab506e7f96f69c535c2128
Vyacheslav Beltyukov <vyacheslav.beltyukov@palta.com>
2022-05-30 10:09:38 +0300

    Fix package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let name: String = "PaltaLib"
 
 let platforms: [SupportedPlatform] = [
-    .iOS(.v10)
+    .iOS(.v11)
 //    .tvOS(.v9),
 //    .macOS(.v10_10)
 ]
@@ -39,7 +39,7 @@ let products: [Product] = [
 
 let dependencies: [Package.Dependency] = [
     .package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS.git", from: "8.5.0"),
-    .package(name: "Purchases", url: "https://github.com/RevenueCat/purchases-ios.git", from: "3.13.0"),
+    .package(name: "RevenueCat", url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.4.0"),
     .package(name: "AppsFlyerLib", url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.4.2")
 ]
 
@@ -59,7 +59,7 @@ let targets: [Target] = [
     .target(
         name: "PaltaLibPurchases",
         dependencies: [
-            "Purchases",
+            "RevenueCat",
             "PaltaLibCore",
             "PaltaLibAttribution"
         ],
