fe1c872f8e745ce8d48cbd5993d20e7f17e26a2f
Vyacheslav Beltyukov <vyacheslav.beltyukov@palta.com>
2022-08-16 14:15:48 +0300

    Code cleanup

--- Example App/PaltaExample/Flow/Purchases/Purchases/PurchasesViewModel.swift
+++ Example App/PaltaExample/Flow/Purchases/Purchases/PurchasesViewModel.swift
@@ -11,15 +11,7 @@ import PaltaLibPurchases
 
 protocol PurchasesViewModelInterface {
     var subscriptionStatePublisher: AnyPublisher<String?, Never> { get }
-
-    var isSubscribeButtonActivePublisher: AnyPublisher<Bool, Never> { get }
-    var isBuyLifetimeButtonActivePublisher: AnyPublisher<Bool, Never> { get }
-    var isBuyPeriodButtonActivePublisher: AnyPublisher<Bool, Never> { get }
     var isButtonActive: AnyPublisher<Bool, Never> { get }
-
-    func subscribe()
-    func buyLifetime()
-    func buyPeriod()
 }
 
 final class PurchasesViewModel: PurchasesViewModelInterface {
@@ -42,27 +34,6 @@ final class PurchasesViewModel: PurchasesViewModelInterface {
             .eraseToAnyPublisher()
     }
 
-    var isSubscribeButtonActivePublisher: AnyPublisher<Bool, Never> {
-        Publishers
-            .CombineLatest(isButtonActive, $subscriptionProduct)
-            .map { $0 && $1 != nil }
-            .eraseToAnyPublisher()
-    }
-    
-    var isBuyPeriodButtonActivePublisher: AnyPublisher<Bool, Never> {
-        Publishers
-            .CombineLatest(isButtonActive, $periodProduct)
-            .map { $0 && $1 != nil }
-            .eraseToAnyPublisher()
-    }
-    
-    var isBuyLifetimeButtonActivePublisher: AnyPublisher<Bool, Never> {
-        Publishers
-            .CombineLatest(isButtonActive, $lifetimeProduct)
-            .map { $0 && $1 != nil }
-            .eraseToAnyPublisher()
-    }
-    
     var isButtonActive: AnyPublisher<Bool, Never> {
         Publishers
             .CombineLatest($state, $userId)
@@ -83,65 +54,10 @@ final class PurchasesViewModel: PurchasesViewModelInterface {
     @Published
     private var userId: UUID?
     
-    @Published
-    private var subscriptionProduct: Product?
-    @Published
-    private var lifetimeProduct: Product?
-    @Published
-    private var periodProduct: Product?
-    
     private var cancels: Set<AnyCancellable> = []
     
     init() {
         setupBindings()
-    }
-
-    func subscribe() {
-        guard let subscriptionProduct = subscriptionProduct else {
-            return
-        }
-
-        PaltaPurchases.instance.purchase(subscriptionProduct, with: nil) { [weak self] result in
-            switch result {
-            case .success(let purchase):
-                self?.update(with: purchase.paidFeatures)
-                
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func buyPeriod() {
-        guard let periodProduct = periodProduct else {
-            return
-        }
-
-        PaltaPurchases.instance.purchase(periodProduct, with: nil) { [weak self] result in
-            switch result {
-            case .success(let purchase):
-                self?.update(with: purchase.paidFeatures)
-                
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func buyLifetime() {
-        guard let lifetimeProduct = lifetimeProduct else {
-            return
-        }
-
-        PaltaPurchases.instance.purchase(lifetimeProduct, with: nil) { [weak self] result in
-            switch result {
-            case .success(let purchase):
-                self?.update(with: purchase.paidFeatures)
-                
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
     
     private func setupBindings() {
@@ -158,18 +74,6 @@ final class PurchasesViewModel: PurchasesViewModelInterface {
     
     private func getPurchases(with userId: UUID) {
         PaltaPurchases.instance.logIn(appUserId: .uuid(userId)) { _ in
-            PaltaPurchases.instance.getProducts(with: ["com.palta.brain.demo.paidfeature"]) { [weak self] result in
-                switch result {
-                case .success(let products):
-                    print("EBUNAAA \(products)")
-                    self?.subscriptionProduct = products.first(where: { $0.productType == .autoRenewableSubscription })
-                    self?.lifetimeProduct = products.first(where: { $0.productType == .nonConsumable })
-                    self?.periodProduct = products.first(where: { $0.productType == .nonRenewableSubscription })
-                case .failure(let error):
-                    print("EBUNAAA \(error)")
-                    print(error)
-                }
-            }
         }
         
         PaltaPurchases.instance.getPaidFeatures { [weak self] result in
@@ -182,10 +86,6 @@ final class PurchasesViewModel: PurchasesViewModelInterface {
                 print(error)
             }
         }
-        
-        print("EBUNAAA")
-        
-        
     }
     
     private func update(with paidFeatures: PaidFeatures) {
