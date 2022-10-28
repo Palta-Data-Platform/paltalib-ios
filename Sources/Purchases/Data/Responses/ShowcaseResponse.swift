7bf0eb31695ecc884c6559951cf1dd5389930441
Vyacheslav Beltyukov <vyacheslav@beltyukov.mobi>
2022-10-28 15:24:30 +0300

    Allow faulty price points in backend response

--- Sources/Purchases/Data/Responses/ShowcaseResponse.swift
+++ Sources/Purchases/Data/Responses/ShowcaseResponse.swift
@@ -7,7 +7,44 @@
 
 import Foundation
 
-struct ShowcaseResponse: Decodable {
+struct ShowcaseResponse {
     let status: String
     let pricePoints: [PricePoint]
+}
+
+extension ShowcaseResponse: Decodable {
+    enum CodingKeys: CodingKey {
+        case status
+        case pricePoints
+    }
+    
+    init(from decoder: Decoder) throws {
+        var container = try decoder.container(keyedBy: CodingKeys.self)
+        var ppContainer = try container.nestedUnkeyedContainer(forKey: .pricePoints)
+        
+        var pricePoints: [PricePoint] = []
+        
+        while !ppContainer.isAtEnd {
+            guard let element = try? ppContainer.decode(PricePoint.self) else {
+                ppContainer.skip()
+                continue
+            }
+            
+            pricePoints.append(element)
+        }
+        
+        self.status = try container.decode(String.self, forKey: .status)
+        self.pricePoints = pricePoints
+    }
+}
+
+private extension UnkeyedDecodingContainer {
+    mutating func skip() {
+        _ = try? decode(DummyValue.self)
+    }
+}
+
+private struct DummyValue: Decodable {
+    init(from decoder: Decoder) throws {
+    }
 }
