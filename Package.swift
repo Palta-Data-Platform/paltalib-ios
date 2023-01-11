5c499cb0b12673cacfb37816f4d87c4fb09a3d19
Vyacheslav Beltyukov <vyacheslav@beltyukov.mobi>
2023-01-11 16:55:56 +0200

    Distribute analytics with SPM

--- Package.swift
+++ Package.swift
@@ -6,23 +6,9 @@ let name: String = "PaltaLib"
 
 let platforms: [SupportedPlatform] = [
     .iOS(.v11)
-//    .tvOS(.v9),
-//    .macOS(.v10_10)
 ]
 
 let products: [Product] = [
-    .library(
-        name: "PaltaLibAttribution",
-        targets: [
-            "PaltaLibAttribution"
-        ]
-    ),
-    .library(
-        name: "PaltaLibPurchases",
-        targets: [
-            "PaltaLibPurchases"
-        ]
-    ),
     .library(
         name: "PaltaLibAnalytics",
         targets: [
@@ -34,19 +20,11 @@ let products: [Product] = [
         targets: [
             "PaltaLibCore"
         ]
-    ),
-    .library(
-        name: "AnalyticsTools",
-        targets: [
-            "AnalyticsTools"
-        ]
     )
 ]
 
 let dependencies: [Package.Dependency] = [
-    .package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS.git", from: "8.5.0"),
-    .package(name: "RevenueCat", url: "https://github.com/RevenueCat/purchases-ios.git", from: "4.4.0"),
-    .package(name: "AppsFlyerLib", url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.4.2")
+    .package(name: "Amplitude", url: "https://github.com/amplitude/Amplitude-iOS.git", from: "8.5.0")
 ]
 
 let targets: [Target] = [
@@ -56,32 +34,12 @@ let targets: [Target] = [
         path: "Sources/Core"
     ),
     .target(
-        name: "PaltaLibAttribution",
-        dependencies: [
-            "AppsFlyerLib"
-        ],
-        path: "Sources/Attribution"
-    ),
-    .target(
-        name: "PaltaLibPurchases",
-        dependencies: [
-            "RevenueCat",
-            "PaltaLibCore",
-            "PaltaLibAttribution"
-        ],
-        path: "Sources/Purchases"
-    ),
-    .target(
         name: "PaltaLibAnalytics",
         dependencies: [
             "Amplitude",
             "PaltaLibCore"
         ],
         path: "Sources/Analytics"
-    ),
-    .target(
-        name: "AnalyticsTools",
-        path: "Sources/AnalyticsTools"
     )
 ]
 
